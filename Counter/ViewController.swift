06c25758f94281ebca292894046c9df19c68db80
«apereboeva» <pereboevaana@ya.ru>
2024-06-02 18:35:13 +0400

    Базовая задача по спринту 3

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var clickCounter: UILabel!
+    
+    
+    var count = 0
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        count += 1
+        clickCounter.text = "Значение счетчика: \(count)"
+    }
+    
 }
 
